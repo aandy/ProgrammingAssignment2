42b1903deacee3c0bb35d7a11699759d3003e422
aandy <anand.ankur@gmail.com>
2014-12-14 18:00:12 +0530

    Programming Assignment 2
    Submission for final evaluation

--- cachematrix.R
+++ cachematrix.R
@@ -13,46 +13,37 @@
 ##function and finally retrun the matrix inverse]
 ##
 ##Code has also been heavily commented to provide the detailed flow of the R program.
-
-makeCacheMatrix <- function(x = matrix()) {      # input x will be a matrix
-
-    m <- matrix()    #  m will be our inverse and it's reset to NULL matrix every 
-                 #    time makeCacheMatrix is called
-
-                 #  note these next three functions are defined but not run when makeCacheMatrix is called.
-                 #   instead, they will be used by cacheSolve() to get values for x or for
-                 #   m (inverse) and for setting the inverse.  These are usually called object 'methods'
-    set <- function(y = matrix()) {	# takes an input matrix
-                x <<- y		# saves the input input matrix in global environment
-                m <<- matrix()	# resets the inverse to 0*0 matrix, which is what happens when a new object is generated via the makeCacheMatrix.
+makeCacheMatrix <- function(x = matrix()) {   # input x will be a matrix
+    m <- matrix()    			      #  m will be our inverse and it's reset to NULL matrix every 
+                 			      #  time makeCacheMatrix is called
+                 			      #  note these next three functions are defined but not run when makeCacheMatrix is called.
+                 			      #  instead, they will be used by cacheSolve() to get values for x or for
+                 			      #  m (inverse) and for setting the inverse.  These are usually called object 'methods'
+    set <- function(y = matrix()) {	      # takes an input matrix
+                x <<- y			      # saves the input input matrix in global environment
+                m <<- matrix()		      # resets the inverse to 0*0 matrix, which is what happens when a new object is generated via the makeCacheMatrix.
         }
-    get <- function() { x }   # this function returns the value of the original matrix
-
+    get <- function() { x }   		      # this function returns the value of the original matrix
     setinverse <- function(inverse)  { m <<- inverse }
-                # this is called by cacheSolve() during the first cacheSolve()
-                                #  access and it will store the value using superassignment
-                                       
-    getinverse <- function() { m } # this will return the cached matrix to cacheSolve() on
-                                #  subsequent accesses
-
-    list(set = set, get = get,          #  This is accessed each time makeCacheMatrix() is called,       
-         setinverse = setinverse,  #   that is, each time we make a new object.  This is a list of 
-         getinverse = getinverse)  #   the internal functions ('methods') so a calling function
-                             #   knows how to access those methods.                            
+                			      # this is called by cacheSolve() during the first cacheSolve()
+                                	      # access and it will store the value using superassignment                                       
+    getinverse <- function() { m } 	      # this will return the cached matrix to cacheSolve() on
+                                	      # subsequent accesses
+    list(set = set, get = get,         	      # This is accessed each time makeCacheMatrix() is called,       
+         setinverse = setinverse,  	      # that is, each time we make a new object.  This is a list of 
+         getinverse = getinverse)  	      # the internal functions ('methods') so a calling function
+                             		      # knows how to access those methods.                            
 }
 
-
-cacheSolve <- function(x, ...) {   # the input x is an object created by makeCacheMatrix
-    m <- x$getinverse()               # accesses the object 'x' and gets the value of the inverse
-    if(!identical(m,matrix())) {              # if inverse was already cached (not IDENTICAL to NULL matrix) ...
-
-        message("getting cached data")  # ... send this message to the console
-        return(m)                       # ... and return the inverse ... "return" ends 
-                        #   the function cacheSolve()
-    }
-    data <- x$get()        # we reach this code only if x$getinverse() returned Identical NULL matrix
-    m <- solve(data, ...)   # if m was identical NULL matrix then we have to calculate the inverse
-    x$setinverse(m)           # store the calculated inverse value in x (see setinverse() in makeCacheMatrix
-    m               # return the inverse to the code that called this function
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {       	      # the input x is an object created by makeCacheMatrix
+    m <- x$getinverse()                	      # accesses the object 'x' and gets the value of the inverse
+    if(!identical(m,matrix())) {       	      # if inverse was already cached (not IDENTICAL to NULL matrix)...
+        message("getting cached data")        # ...send this message to the console
+        return(m)                      	      # ... and return the inverse. The "return" ends the function cacheSolve()
+				}
+    data <- x$get()        		      # we reach this code only if x$getinverse() returned Identical NULL matrix
+    m <- solve(data, ...)   		      # if m was identical NULL matrix then we have to calculate the inverse
+    x$setinverse(m)           		      # store the calculated inverse value in x (see setinverse() in makeCacheMatrix
+    m               			      # return the inverse to the code that called this function
+        				      ## Return a matrix that is the inverse of 'x'
 }
